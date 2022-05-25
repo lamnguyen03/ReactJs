be6d68456308a8013d22622e51911a4cf246fe7a
unknown <admin@pnvd191.pn.com.vn>
2022-05-25 19:45:36 +0700

    up

---
+++ src/Router/Routes.js
@@ -0,0 +1,4 @@
+import About from "./About";
+import Home from "./Home";
+import Contact from "./Contact";
+
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import Father from './Router/Father';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Father/>
+  <Crud/>
   </React.StrictMode>
 );
 
