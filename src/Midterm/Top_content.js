c2606d9461d7eb7fec2409bfbc40ec66f48a1329
unknown <admin@pnvd191.pn.com.vn>
2022-05-28 11:21:48 +0700

    up

--- src/Midterm/Top_content.js
+++ src/Midterm/Top_content.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Top_content extends Component {
-
-
+  constructor(props){
+    super(props);
+    this.state = {
+       news: []
+        
+    };
+    
+    this.componentDidMount = this.componentDidMount.bind(this);
+   
+  }
+  componentDidMount() {  
+    axios.get(`https://61bc10c1d8542f0017824535.mockapi.io/Midterm`)
+    .then(res => {
+      const news = res.data;
+      this.setState({news});
+    })
+    .catch(error => console.log(error));
+  }
+/*{listProduct
+.filter((products) => products.status === "outstanding")
+.map((product, index) => (
+  <Outstanding
+    key={index}
+    image={product.image_post}
+    title={product.title_post}
+  />
+))}*/
   render() {
     return (
       <div className="container">
-         <div>
-          <div className="col-md-6">
-              <p>lídjglsdfg</p>
-          </div>
-
+         <h2>THẾ GIỚI</h2>
+         <br></br>
+         <div className="row">
             <div className="col-md-6">
-              <p>lídjglsdfg</p>
-
+              {
+              this.state.news[this.state.news.length-1] !== undefined?
+              <div className='left'>
+                <img src={this.state.news[this.state.news.length-1].img} alt="Lamp" ></img> <br></br>
+                {this.state.news[this.state.news.length-1].title}
+              </div> 
+              :
+              console.log(2222)
+              }
             </div>
+            <div className="col-md-6">
+            {this.state.news.map((news)=>(
+              <div className='row'>
+                <div className='col-md-4'>
+                <img src={news.img} alt="Lamp" ></img>
+                </div>
+                <div className='col-md-8'>
+                  {news.title}
+                </div>
+              </div>
+          ))}
+          
+            </div>
+             
+          
          </div>
       </div>
     )
